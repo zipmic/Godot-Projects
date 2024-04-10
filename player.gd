b804be03885fc12aca0917b1e3312b9d19c9d890
zipmic <mail@code.dk>
2024-04-10 10:58:01 +0200

    stuff

---
+++ player.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@export var speed = 350
+var velocity = Vector2.ZERO
+var screensize = Vector2(480,720)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	position += velocity * speed * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	
+	if velocity.length() > 0:
+		$AnimatedSprite2D.animation = "run"
+	else:
+		$AnimatedSprite2D.animation = "idle"
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
